67c75af4fb594370a32ba46d33bcaf0a03948b0a
DURET Arthur <arthur.duret@arlovic.fr>
2025-12-02 14:10:17 +0100

    Implémentation de ObservateurGrille.hpp

--- Projet/ObservateurGrille.hpp
+++ Projet/ObservateurGrille.hpp
@@ -1,8 +1,15 @@
 #ifndef OBSERVATEURGRILLE_H
 #define OBSERVATEURGRILLE_H
 
+class Grille;
 
+class ObservateurGrille {
+    protected :
 
+    public :
+        virtual ~ObservateurGrille() = default; // Destructeur
+        virtual void notifierChangement(const Grille& grille) = 0; //
+}
 
 
 #endif
