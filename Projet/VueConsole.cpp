641a37eb7a7ff08100801c36d5409d30509ba5e5
DURET Arthur <arthur.duret@arlovic.fr>
2025-12-02 14:43:08 +0100

    feat: Implémentation de la logique d'affichage de VueConsole

--- Projet/VueConsole.cpp
+++ Projet/VueConsole.cpp
@@ -1,1 +1,21 @@
 #include <VueConsole.hpp>
+#include <iostream>
+#include <Grille.hpp>
+using namespace std;
+
+VueConsole::notifierChangement(const Grille& grille){
+    cout << "--- Nouvelle Génération ---" << endl;
+    for (size_t y = 0; y < grille.getHauteur(); y++) {
+        for (size_t x = 0; x < grille.getLargeur; x++) {
+            if (grille.getCellule(x, y).estVivante()) {
+                cout << "■";
+            }
+            else {
+                cout << "□";
+            }
+        }
+        cout << endl;
+    }
+};
+
+
