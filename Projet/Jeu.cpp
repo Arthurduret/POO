a274d3e96e9ce1f27a80a9e278e89032c5bd013a
DURET Arthur <arthur.duret@arlovic.fr>
2025-12-03 15:52:27 +0100

    Jeu finis + main

--- Projet/Jeu.cpp
+++ Projet/Jeu.cpp
@@ -1,3 +1,75 @@
 #include "Jeu.hpp"
+#include "Grille.hpp"
 #include <iostream>
+#include <fstream>
+using namespace std;
+using GridData = vector<vector<int>>;
 
+GridData lireConfiguration(const string& nomFichier) {
+    GridData configuration; 
+    ifstream fichier(nomFichier);
+    if (!fichier.is_open()) {   
+    throw runtime_error("Impossible d'ouvrir le fichier de configuration : " + nomFichier);
+    }
+    string ligne;
+    while (getline(fichier, ligne)) {
+        vector<int> row;
+        for (char c : ligne) {
+            if (c == '1' || c == 'X') {
+                row.push_back(1); // Cellule vivante
+            } else if (c == '0' || c == '.') {
+                row.push_back(0); // Cellule morte
+            }
+        }
+        if (!row.empty()) {
+            configuration.push_back(row);
+        }
+    }
+
+    return configuration;
+}
+
+JeuDeLaVie::JeuDeLaVie(const string& nomFichierConfig) {
+    GridData config = lireConfiguration(nomFichierConfig);
+    
+    if (config.empty() || config[0].empty()) {
+        grille = nullptr;
+        throw runtime_error("Le fichier de configuration est vide.");
+    }
+
+    // Étape 2 : Calculer la hauteur et la longueur
+    int hauteur = config.size();         
+    int longueur = config[0].size(); 
+
+    this->grille = new Grille(longueur, hauteur); 
+
+    // Étape 4 : INITIALISER les Cellules
+    // On appelle la méthode init() de l'objet Grille pour qu'il
+    // remplisse sa matrice de Cellule avec les états 1 ou 0.
+    this->grille->init(config);
+}
+
+JeuDeLaVie::~JeuDeLaVie() {
+    delete grille;
+}
+
+void JeuDeLaVie::ajouterObservateur(ObservateurGrille* vue) {
+    observateurs.push_back(vue);
+}
+
+void JeuDeLaVie::lancer(int generations) {
+    if (!grille) {
+        cerr << "Erreur: Le jeu ne peut pas être lancé car la grille est invalide." << endl;
+        return;
+    }
+
+    cout << "Demarrage du Jeu de la Vie de Conway pour " << generations << " generations.\n";
+
+    for (int i = 0; i<generations; i++) {
+        cout << "n\--- Generation " << i + 1 << "---" << endl;
+
+        for (ObservateurGrille* obs : observateurs) {
+            obs->notifierChangement(*grille);
+        }
+    }
+}
--- Projet/Jeu.hpp
+++ Projet/Jeu.hpp
@@ -2,12 +2,26 @@
 #define JEU_H
 
 #include "Grille.hpp"
+#include "ObservateurGrille.hpp"
 #include <vector>
+#include <string>
+using namespace std;
 
 
 
+class JeuDeLaVie {
+    private : 
+        Grille* grille;
+        vector<ObservateurGrille*> observateurs;
 
+    public :
+        JeuDeLaVie(const string& nomFichierConfig);
 
+        ~JeuDeLaVie();
 
+        void ajouterObservateur(ObservateurGrille* obs);
+
+        void lancer(int generations);
+};
 
 #endif
